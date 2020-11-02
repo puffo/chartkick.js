fd277ea0b528119fdc9ed6dc7c818f52dacb3420
Andrew Kane <andrew@chartkick.com>
2020-11-01 19:53:06 -0800

    Added destroyAll method

--- src/index.js
+++ src/index.js
@@ -468,6 +468,9 @@ class Chart {
   }
 
   destroy() {
+    this.destroyed = true;
+    this.stopRefresh();
+
     if (this.__adapterObject) {
       this.__adapterObject.destroy(this);
     }
@@ -620,6 +623,14 @@ const Chartkick = {
     for (let chartId in Chartkick.charts) {
       if (Chartkick.charts.hasOwnProperty(chartId)) {
         callback(Chartkick.charts[chartId]);
+      }
+    }
+  },
+  destroyAll: function() {
+    for (let chartId in Chartkick.charts) {
+      if (Chartkick.charts.hasOwnProperty(chartId)) {
+        Chartkick.charts[chartId].destroy();
+        delete Chartkick.charts[chartId];
       }
     }
   },
